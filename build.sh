1564e5090a107f3344e81153f0f50ffb7fa6ae04
Rui Rua <rui.rrua@gmail.com>
2020-05-22 16:07:50 +0100

    bug fix

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ CMAKE_VERSION=$(find ${ANDROID_HOME}/cmake/  ! -path ${ANDROID_HOME}/cmake/  -ma
 #check if cmake is installed
 test -z ${CMAKE_VERSION} && echo "CMAKE not installed: visit https://developer.android.com/studio/projects/install-ndk in order to properly install this tool" && exit 1
 
+mkdir -p build
 cd build
 ${CMAKE_VERSION}/bin/cmake ../ -DCMAKE_TOOLCHAIN_FILE=${ANDROID_HOME}/ndk-bundle/build/cmake/android.toolchain.cmake -DANDROID_ABI=${DEVICE_ABI} -DANDROID_NATIVE_API_LEVEL=${DEVICE_API_LEVEL}
 cd ..
