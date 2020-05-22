12c851e78815cf4b85c4592672da255f89160a10
Rui Rua <rui.rrua@gmail.com>
2020-05-22 16:00:22 +0100

    proj reestructure; added build script to generate replay executable using cmake

---
+++ build/CMakeFiles/foobar.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/foobar"
+  "foobar.jar"
+  "CMakeFiles/foobar.dir/java_class_filelist"
+  "CMakeFiles/foobar.dir/java_compiled_foobar"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/foobar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
